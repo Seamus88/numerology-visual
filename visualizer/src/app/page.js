0198d379d0b62e8c03f573fd6e3a5c55a72ff2b6
James Cosgrove <Seamus.Coz@gmail.com>
2025-04-27 16:48:55 +0000

    Added BgColor to tdCells, added repeating table headers between years for day reference

--- visualizer/src/app/page.js
+++ visualizer/src/app/page.js
@@ -1,34 +1,47 @@
 
 "use client";
+import { Hepta_Slab, Ruge_Boogie } from "next/font/google";
 import React, { useState } from "react";
 
 
 export default function Home() {
-  const [dates, setDates] = useState({fromDate: 0, toDate: 0, dateArray: [], show: false});
+  const [dates, setDates] = useState({fromDate: "", toDate: "", dateArray: [], show: false});
   
   function makeTable() {
     const fromDate = new Date(dates.fromDate);
     const toDate = new Date(dates.toDate);
-
+    const datesArrayTemp = [];
+    let holdYear = 0;
+    let holdMonth = 0;
+    
     if (!dates.fromDate || !dates.toDate) {
       alert("Please select both dates.");
       return;
     } else if (fromDate > toDate) {
-      alert("The 'From Date' must be before the 'To Date'.");
+      alert("Please select choices in chronological order.");
       return;
-    } else if (fromDate - toDate > 600) {
-      alert("The date range is too large. Please select a smaller range.");
-      return;
-    }
-
-
-    const dateArray = [];
+    } 
+    
     for (let i = fromDate; i <= toDate; i.setDate(i.getDate() + 1)) {
-      dateArray.push(new Date(i));
+      const year = i.getFullYear();
+      const month = i.getMonth() + 1;
+
+      if (holdYear !== year && holdMonth !== month) {
+        holdYear = year;
+        holdMonth = month;
+        datesArrayTemp.push([0, 0]);
+        datesArrayTemp.push([year, month]);
+      } else if (holdYear === year && holdMonth !== month) {
+        holdMonth = month;
+        datesArrayTemp.push([year, month]);
+      } else if (holdYear === year && holdMonth === month) {
+        continue;
+      }
     }
-    console.log(dateArray)
+
     setDates((prevState) => ({
       ...prevState,
+      dateArray: [...datesArrayTemp],
       show: !prevState.show,
     }));
   }
@@ -81,42 +94,91 @@ export default function Home() {
 }
 
 function CreateTable({dates}) {
-  const monthsAndDays = {"January": 31, "February": 28, "March": 31, "April": 30, "May": 31, "June": 30, "July": 31, "August": 31, "September": 30, "October": 31, "November": 30, "December": 31};
-  const monthNames = Object.keys(monthsAndDays);
-  const monthDays = [1,2,3,4,5,5,6,7,8,9,10,11,12, 13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
-  const datesArray = [];
-  
-
-
-  function calculateLifeLesson(date) {
-    const dateArray = date.split("-");
-    const year = dateArray[0].split("");
-    let yearSum = 0;
-    year.forEach((num) => yearSum += parseInt(num));
-    const month = parseInt(dateArray[1]);
-    const day = parseInt(dateArray[2]);
-    return yearSum + month + day;   
-  }
+  const days = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
   
   return (
-  <div className="flex flex-col gap-4 justify-center  items-center">
-      <h1 className="p-2">{dates.fromDate} - {dates.toDate}</h1>
-      <table className="border-collapse border border-gray-300">
+    <div className="overflow-x-auto">
+      <table className="min-w-full border border-gray-600">
         <thead>
           <tr>
-            <th className="border border-gray-300 p-2" colSpan="1"></th>
-            <th className="border border-gray-300 p-2" colSpan="32">Days</th>
+            <th className="border border-gray-600 p-2" colSpan="33">{dates.fromDate} to {dates.toDate}</th>
           </tr>
           <tr>
-            <th className="border border-gray-300 p-2">Month</th>
-            {monthDays.map((day, index) => <th key={index} className="border-gray-300 p-2">{day}</th>)}
+            <th className="border border-gray-600 p-2">Year</th>
+            <th className="border border-gray-600 p-2">Month</th>
+            {days.map((day) => (
+              <th key={day} className="border border-gray-600 p-2">{day}</th>
+            ))}
           </tr>
         </thead>
-        <tbody>
-            
-        </tbody>
+        <TBody dateArray={dates.dateArray} />
       </table>
     </div>
+  );
+}
 
+function TBody({dateArray}) {
+  const datesToMonths = {"0": 31, "1": 31, "2": 28, "3": 31, "4": 30, "5": 31, "6": 30, "7": 31, "8": 31, "9": 30, "10": 31, "11": 30, "12": 31};  
+
+  return (
+    <tbody>
+      {dateArray.map((yearAndMonth, index) => {
+        return (
+          <TR key={index} date={yearAndMonth} datesToMonths={datesToMonths[yearAndMonth[1].toString()]} />
+        )
+      })}
+    
+    </tbody>
   )
 }
+
+function TR({date, datesToMonths}) {
+  const daysInMonth = [];
+  for(let i = 1; i <= datesToMonths; i++) {
+    daysInMonth.push(i);
+  }
+  
+
+  return (
+    <tr>
+      {date[0] === 0? <td className="border border-gray-600">Year</td> : <td className="border border-gray-600 p-2">{date[0]}</td>}
+      {date[1] === 0? <td className="border border-gray-600">Month</td> : <td className="border border-gray-600 p-2">{date[1]}</td>}
+      {daysInMonth.map((day) => {
+        return (
+          <TD key={day} year={date[0]} month={date[1]} day={day} />
+        )
+      })}
+    </tr>
+  )
+}
+
+function TD({year,month,day}) {
+  if (year === 0) {
+    return (
+      <td className="border border-gray-600 p-2">{day}</td>
+    )
+  }
+  let yearReduced = year.toString().split("");
+  yearReduced = yearReduced.map((num) => {
+    return parseInt(num);
+  }); 
+  const llNumber = yearReduced[0] + yearReduced[1] + yearReduced[2] + yearReduced[3] + month + day;
+  let llNumberReduced = llNumber.toString().split("");
+  llNumberReduced = llNumberReduced.map((num) => {
+    return parseInt(num);
+  });
+  while (llNumberReduced.length > 1) {
+    let sum = 0;
+    llNumberReduced.forEach((num) => {
+      sum += num;
+    });
+    llNumberReduced = sum.toString().split("");
+    llNumberReduced = llNumberReduced.map((num) => {
+      return parseInt(num);
+    });
+  }
+  const llNumbRed = llNumberReduced[0];
+  return (
+    <td className="border border-gray-600 p-2" style={{backgroundColor: "hsl("+llNumber+", "+(llNumbRed*10)+"%, 50%)"}}>{llNumber}/{llNumbRed}</td>
+  )
+}  
